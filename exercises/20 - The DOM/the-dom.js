5e6099a15b2eda794814642df763c732cd7aba04
brandonperfetti <brandon@brandonperfetti.com>
2019-12-04 18:34:30 -0800

    Complete module 4, Lesson 22: The DOM - Element Properties and Methods.

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -4,7 +4,28 @@ const p = document.querySelector('p');
 const imgs = document.querySelectorAll('.item img');
 const item2 = document.querySelector('.item2');
 const item2image = item2.querySelector('img');
-console.log(p);
-console.log(imgs);
+const heading = document.querySelector('h2');
+
+console.log(heading.textContent);
+console.log(heading.innerText);
+//  set the textContent property on that element
+// heading.textContent = 'Brandon is cool';
+// console.log(heading.textContent);
+// console.log(heading.innerText);
 
 // document.addEventListener('DQMContentLoaded', init);
+
+console.log(heading.innerHTML);
+console.log(heading.outerHTML);
+
+//  get properties from the element
+const pizzaList = document.querySelector('.pizza');
+//  set properties on the element
+console.log(pizzaList.textContent);
+
+// use a bit more powerful methods on each of our elements/nodes
+
+// pizzaList.textContent = `${pizzaList.textContent}🍕`;
+pizzaList.insertAdjacentText('beforeend', '🍕');
+// pizzaList.insertAdjacentText('beforebegin', '🍕');
+pizzaList.insertAdjacentText('afterbegin', '🍕');
