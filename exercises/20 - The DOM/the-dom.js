2ceec4d32276432ad0f7f47982c136b4a8415b3d
brandonperfetti <brandon@brandonperfetti.com>
2019-12-04 17:29:49 -0800

    Complete module 4, Lesson 21: The DOM - Selecting Elements.

---
+++ exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,10 @@
+//  will only ever give you the first matching element
+const p = document.querySelector('p');
+// will give you multiple elements
+const imgs = document.querySelectorAll('.item img');
+const item2 = document.querySelector('.item2');
+const item2image = item2.querySelector('img');
+console.log(p);
+console.log(imgs);
+
+// document.addEventListener('DQMContentLoaded', init);
